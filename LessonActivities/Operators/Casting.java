bd3f44c54874c1a3dabe2f8b56e02facf42f8335
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-11-30 11:42:34 -0500

    initial practice of casting

---
+++ LessonActivities/Operators/Casting.java
@@ -0,0 +1,10 @@
+package LessonActivities.Operators;
+
+public class Casting {
+    public static void main(String[] args) {
+        System.out.println("Float to int:");
+        float f = 3.99F;
+        int i = (int)f;
+        System.out.println(i);
+    }
+}
