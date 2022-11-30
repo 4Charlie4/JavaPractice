46e24a77438f5c7b349a48c6a8c5ef9faa278b8d
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-11-30 11:52:06 -0500

    Casting various datatypes

--- LessonActivities/Operators/Casting.java
+++ LessonActivities/Operators/Casting.java
@@ -4,7 +4,21 @@ public class Casting {
     public static void main(String[] args) {
         System.out.println("Float to int:");
         float f = 3.99F;
-        int i = (int)f;
+        int i = (int)f; // no rounding occurs
         System.out.println(i);
+
+        System.out.println("int to float:");
+        f = i; //upcasting is implicit(Java does it for you) which is fitting a smaller datatype into a larger datatype. 
+        System.out.println(f);
+
+        int x = 10;
+        float y = 3.755F;
+        System.out.println(x + y); //When mathematical operations are performed Java auto Upcasts
+
+        char c = 'A';
+        System.out.println((int)c); //casting can occur between any primitive data EXCEPT Boolean.
+
+        char z = 'Z';
+        System.out.println((byte)z);
     }
 }
