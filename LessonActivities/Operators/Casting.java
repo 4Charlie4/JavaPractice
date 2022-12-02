e9080096b43231d736e7c773585f2fce7a355f13
Charles Duran <62945929+4Charlie4@users.noreply.github.com>
2022-12-02 11:14:33 -0500

    updating repo

--- LessonActivities/Operators/Casting.java
+++ LessonActivities/Operators/Casting.java
@@ -2,7 +2,7 @@ package LessonActivities.Operators;
 
 public class Casting {
     public static void main(String[] args) {
-        System.out.println("Float to int:");
+        System.out.println("float to int:");
         float f = 3.99F;
         int i = (int)f; // no rounding occurs
         System.out.println(i);
